715aac2223f3934cd76b9acf7f4626b8ab15aeb5
dongdiemthuy <dongdiemthuy@gmail.com>
2015-04-09 16:23:55 +0700

    save pagination

--- js/addrecipe.js
+++ js/addrecipe.js
@@ -109,52 +109,5 @@ app.factory('nutrientList', function($log) {
 
 app.controller('createRecipeCtrl', ['$scope', '$log', '$http', function($scope, $log, $http) {
     $log.debug('go to createRecipe Controller');
-    // $scope.ingredients = [];
-   
-    //         $scope.init = function() {
-    //           $scope.ingredients = [];
-    //           $scope.query = undefined;
-    //           $scope.currentPage = 1;
-    //           $scope.itemsPerPage = 30;
-    //           $scope.totalItems = -1;
-    //         }
-
-    //         $scope.loadMoreIngredients = function() {
-    //           var myparams = {
-    //               offset: ($scope.currentPage - 1) * $scope.itemsPerPage,
-    //               max: $scope.itemsPerPage
-    //           }
-
-    //           if ($scope.query != undefined && $scope.query != null) {
-    //             myparams['q'] = $scope.query;
-    //           }
-
-    //           $http.get('http://localhost:7777/nutrix-app/ws/nutrix/adm/ingredient', {
-    //               params: myparams
-    //             }).then(function(resp) {
-    //               console.log('Success', resp);
-    //               $log.debug("Start:" + ($scope.currentPage - 1) * $scope.itemsPerPage);
-    //               $scope.totalItems = resp.data.total;
-    //               $log.debug("Total:" + $scope.totalItems);
-    //               $scope.ingredients = resp.data.collection;
-    //               console.log($scope.itemsPerPage);
-    //             }, function(err) {
-    //               console.error('ERR', err);
-    //             });
-    //           };
-
-    //         $scope.$watch('currentPage + itemsPerPage', function() {
-    //           $scope.loadMoreIngredients();
-    //         });
-
-    //         $scope.doSearchIngredients = function(q) {
-    //             console.log('doSearchIngredients');
-    //             $scope.init();
-    //             $scope.query = q;
-    //             $scope.loadMoreIngredients();
-    //         };
-
-    //         $scope.init();
     
 }]);
-
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
+// chi dinh nghia 1 lan, cac directive, controller dung lai thi chi goi 'nutrixApp' khong de them []
 angular.module('nutrixApp', 
 	['ngRoute', 'ui.router',
 	'infinite-scroll','ui.bootstrap',
 	'ui.bootstrap.tpls']);
-
